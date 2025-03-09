5c1b9e5f448ed25e2e40904e3babd8d8ec0cd1a9
suhail shakeel <78268980+suhailshakeel@users.noreply.github.com>
2025-03-09 07:09:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -20,18 +20,23 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import AdminDashboard from './pages/AdminDashboard';
-
 import 'react-toastify/dist/ReactToastify.css';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  React.useEffect(() => window.scrollTo(0, 0), [pathname]);
+  return null;
+};
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <div className="min-h-screen flex flex-col">
+          <ScrollToTop />
           <Header />
           <main className="flex-grow pt-16">
             <Routes>
-              {/* Public Routes */}
               <Route path="/" element={<Home />} />
               <Route path="/rooms" element={<Rooms />} />
               <Route path="/about" element={<About />} />
@@ -43,38 +48,37 @@ function App() {
               <Route path="/signup" element={<SignUp />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
 
-              {/* Protected Routes - Normal Users */}
               <Route path="/book/:roomId" element={
                 <ProtectedRoute allowedRoles={['user']}>
                   <BookingPage />
                 </ProtectedRoute>
               } />
+
               <Route path="/transactions" element={
                 <ProtectedRoute allowedRoles={['user']}>
                   <Transactions />
                 </ProtectedRoute>
               } />
 
-              {/* Protected Routes - Home Stayers */}
               <Route path="/create-ad" element={
                 <ProtectedRoute allowedRoles={['home-stayer']}>
                   <CreateAd />
                 </ProtectedRoute>
               } />
 
-              {/* Protected Routes - All Authenticated Users */}
               <Route path="/profile" element={
                 <ProtectedRoute>
                   <Profile />
                 </ProtectedRoute>
               } />
 
-              {/* Admin Routes */}
               <Route path="/admin/*" element={
                 <ProtectedRoute allowedRoles={['admin']}>
                   <AdminDashboard />
                 </ProtectedRoute>
               } />
+
+              <Route path="*" element={<Home />} />
             </Routes>
           </main>
           <Footer />
